28c3ffdd0efdbac7271946a3f7049df95ad19e14
ecoyamas <hayato53pu@gmail.com>
2021-07-22 15:45:14 +0900

    新規登録画面作成

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -3,4 +3,21 @@ class CatsController < ApplicationController
   def index
     @cats = Cat.all
   end
+
+  def new
+    @cat = Cat.new
+  end
+
+  def create
+    cat = Cat.new(cat_params)
+    cat.save!
+
+    redirect_to cats_path
+  end
+
+  private
+
+  def cat_params
+    params.require(:cat).permit(:name, :age)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # mount Split::Dashboard, at: 'split'
 
-  # get  "sample/index"  => "sample#index"
-  get "cats" => "cats#index"
+  resources :cats
 end
